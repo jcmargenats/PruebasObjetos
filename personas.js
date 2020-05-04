a57d19c75982f623bd2c5b6dae9b02a1f3ebb4ba
jcmargen <jcmargen@ca.ibm.com>
2020-05-04 12:53:40 -0400

    Segundo connit.

--- personas.js
+++ personas.js
@@ -1,33 +1,38 @@
 //=========  PERSONA  =============
 //  *Creacion de Objeto Padre (Persona)
-function Persona(pila, apellido, edad, genero, intereses){
-    this.nombre = {
-        'pila'    : pila,
-        'apellido': apellido
-    };
+// *otro ejemplo para el commit.
+function Persona(pila, apellido, edad, genero, intereses) {
+  this.nombre = {
+    pila: pila,
+    apellido: apellido,
+  };
 
-    this.edad      = edad;
-    this.genero    = genero;
-    this.intereses = intereses;
-};
+  this.edad = edad;
+  this.genero = genero;
+  this.intereses = intereses;
+}
 //  *Funciones para el Objeto Padre (Persona)
-Persona.prototype.bio = function(){
-        console.log('Yo no soy esa');
+Persona.prototype.bio = function () {
+  console.log("Yo no soy esa");
 };
 //  *Creacion del Objeto Hijo (Profesor)
-function Profesor(pila, apellido, edad, genero, intereses, materias){
-    Persona.call(this, pila, apellido, edad, genero, intereses);
-//  *Parametros adicionales para el Objeto Hijo (Profesor)    
-    this.materias = materias;
-
-};
-Profesor.prototype             = Object.create(Persona.prototype);
+function Profesor(pila, apellido, edad, genero, intereses, materias) {
+  Persona.call(this, pila, apellido, edad, genero, intereses);
+  //  *Parametros adicionales para el Objeto Hijo (Profesor)
+  this.materias = materias;
+}
+Profesor.prototype = Object.create(Persona.prototype);
 Profesor.prototype.constructor = Profesor;
 
 //  *funciones para el Objeto Hijo (Profesor)
-Profesor.prototype.saludo = function(){
-    console.log('Queaces?');
+Profesor.prototype.saludo = function () {
+  console.log("Queaces?");
 };
-const ale = new Profesor('Maria Alejandra', 'Lynch', 
-  60, 'Mujer', ['Musica', 'Lectura', 'Cocina'], ['Fundamentos', 'Matematicas', 'Historia']);
- 
+const ale = new Profesor(
+  "Maria Alejandra",
+  "Lynch",
+  60,
+  "Mujer",
+  ["Musica", "Lectura", "Cocina"],
+  ["Fundamentos", "Matematicas", "Historia"]
+);
